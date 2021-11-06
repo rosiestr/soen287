2378fbbdf8692779213c44a3ae64a4ef74499cec
rosiestr <75342579+rosiestr@users.noreply.github.com>
2021-11-06 17:50:44 -0400

    added more description for all p2

---
+++ description.js
@@ -0,0 +1,8 @@
+function loadDescription() {
+    var x = document.getElementById("expandDesc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
