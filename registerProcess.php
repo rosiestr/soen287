4b968a53892b2eaad1dc14a1340eb7fd7df99747
marouakrim <marouakrim@outlook.com>
2021-12-07 23:27:16 -0500

    more registration

--- registerProcess.php
+++ registerProcess.php
@@ -2,6 +2,7 @@
 
 if(isset($_POST['Register']))
 {
+
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $useremail = $_POST['emailAddress'];
@@ -12,13 +13,19 @@ if(isset($_POST['Register']))
     $zipcode = $_POST['zipcode'];
 
 
+    // $xmltemp = file_get_contents('users.xml');
+    // if(strpos($xmltemp,$useremail)!==false)
+    // {
+    //     echo 'Errooooor';
+    // }
+
+
          $file = "users.xml";
          $simplexml = simplexml_load_file($file);
 
         $users = $simplexml->users; 
         $user= $users->addChild('user');
 
-        //$simplexml = new SimpleXMLElement('<user></user>');
         $user->addChild('firstName',$firstName);
         $user->addChild('lastName',$lastName);
         $user->addChild('useremail',$useremail);
@@ -28,18 +35,16 @@ if(isset($_POST['Register']))
         $user->addChild('province',$province);
         $user->addChild('zipcode',$zipcode);
 
-        // $simplexml->asXML('users/'. $lastName . '.xml');
+        $simplexml->asXML($lastName . $firstName . '.xml');
         $simplexml->asXML($file);
-
-        //header("Location: random.html");
+        header("Location: " .$lastName . $firstName . ".xml");
         die;
         //header("Location: ".$_SERVER['HTTP_REFERER']);
+        //header("Location: random.html");
 
 
+    
 
-  // $xml = simplexml_load_file("xml/users.xml") or die("Error: Cannot create object");
-  // $useremail = $users->useremail; 
-  // $password = $users->md5(passWord);
 }
 
 ?>
